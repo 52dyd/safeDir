e93e13080b72e3ede9dbca7fbff0ffad7d04ba5b
yuwz <yuwz@yuwz.fun>
2022-09-01 10:15:05 -0400

    yes

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /tmp/safedir
 cat /tmp/safedir/static/nginx.conf > /usr/local/nginx/conf/nginx.conf
 cp -r /tmp/safedir/static/wwwphp /home/www-data/
 /usr/local/nginx/sbin/nginx -s reload
