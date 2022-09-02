aef829ac1559791ad7fc872edf2f881c155c253d
nobody <nobody@nobody.com>
2022-09-02 09:20:12 +0800

    yes

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp/safedir
-git pull
+git pull -q
 /tmp/safedir/run.sh
-git checkout main
+git checkout main -q
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+rm -rf /tmp/wwwphp
 cd /tmp/safedir
 cp -r /tmp/safedir/static/wwwphp /tmp/
+/usr/local/nginx/sbin/nginx -s stop
 /usr/local/nginx/sbin/nginx -c /tmp/wwwphp/nginx.conf
-/usr/local/nginx/sbin/nginx -s reload
 
