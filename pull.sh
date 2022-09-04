5c35d20f7d446a0540347b26da593fff64d49e51
nobody <nobody@nobody.com>
2022-09-04 23:36:11 +0800

    yes

--- pull.sh
+++ pull.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp/safedir
 git pull -q
-/tmp/safedir/fuckyang2.sh
+/tmp/safedir/fuckyang2.sh &
 #/tmp/safedir/run.sh
-/tmp/safedir/getflag.sh
+/tmp/safedir/getflag.sh &
 echo test > /tmp/test
 chmod 600 /tmp/test
 sleep 15s
