aea62491c0be96226b5f5611ec49fd932a93a0a4
nobody <nobody@nobody.com>
2022-09-04 10:40:04 +0800

    yes

---
+++ fuckyang.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /var/tmp
+rm -rf /var/tmp/*
+for i in [1..100]
+do
+ps aux|grep www-data|grep -E '/var/tmp'|awk '{print $2}'|xargs kill -9
+done
+rm -rf /var/tmp/*
+echo fuckyou > .yangsid
+echo fuckyou > .dhcp
+chmod 555 .yangsid .dhcp
--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /tmp/safedir
 git pull -q
+/tmp/safedir/fuckyang.sh
 /tmp/safedir/run.sh
 /tmp/safedir/getflag.sh
 sleep 15s
